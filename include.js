d84668e20c8a53529e7ef1602693efb0f669c548
funyapar <funyaper@hotmail.com>
2025-11-09 08:39:17 +0900

    コモンパーツ

---
+++ include.js
@@ -0,0 +1,11 @@
+// assets/js/include.js
+document.addEventListener("DOMContentLoaded", () => {
+  const includes = document.querySelectorAll("[data-include]");
+  includes.forEach(el => {
+    const file = el.getAttribute("data-include");
+    fetch(file)
+      .then(res => res.text())
+      .then(data => el.innerHTML = data)
+      .catch(err => console.error("Include failed:", file, err));
+  });
+});
